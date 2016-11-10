03d97bb0d7b1dd43d31c8d20a7ac106db141a4d2
nemvarsani <nemesh2.varsani@live.uwe.ac.uk>
2016-11-10 13:52:34 +0000

    user
    Created user class

---
+++ src/java/model/User.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author nemeshvarsani
+ */
+public class User {
+    
+    private String id;
+    private String password;
+    private String status;
+
+    
+    //constructor 
+    public User(String id, String password, String status) {
+        this.id = id;
+        this.password = password;
+        this.status = status;
+    }
+
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", password=" + password + ", status=" + status + '}';
+    }
+    
+    
+}
